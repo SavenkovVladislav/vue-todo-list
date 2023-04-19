09550930f7ec90ac941956f98f991faf67d7142f
Vladislav <graffitiopi@gmail.com>
2023-04-19 20:26:33 +0700

    added file AddTodo.vue

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,9 +1,13 @@
 <template>
 	<div>
 		<ul>
+			<!-- Тут мы будем обрабатывать/прослушивать событие remove-todo, которое было создано в дочернем компоненте TodoItem -->
+			<!-- Делается это с помощью дериктивы v-on, где указывается название того события, которое мы описали в TodoItem. Данное событие называется remove-todo -->
+			<!-- В качестве значения v-on:remove-todo необходимо указать метод, который будет выполняться. Назовем его removeTodoMethod и опишем в объекте methods-->
 			<TodoItem
-				v-for="todo in todosProp"
+				v-for="(todo, index) in todosProp"
 				v-bind:todoProp="todo"
+				v-bind:todoIndex="index"
 				v-on:remove-todo="removeTodoMethod"
 			/>
 		</ul>
@@ -18,7 +22,12 @@ export default {
 		TodoItem,
 	},
 	methods: {
+		// Тут создается метод removeTodoMethod, который будет выполняться при прослушивании события remove-todo
+		// Так как в компоненте TodoItem в событие remove-todo мы передали данные, а именно свойство id, то тут мы можем вытащить этот id
+		// Далее мы создаем новое событие, которое передать теперь уже в компонент app и называем его также remove-todo, назвать его можно как угодно
+		// В целом у нас повторяется история с тем, как мы передавали событие из TodoItem в TodoList, только теперь мы хотим передать событие с данными из TodoList в App
 		removeTodoMethod(id) {
+			console.log(id)
 			this.$emit('remove-todo', id)
 		},
 	},
